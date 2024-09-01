00bd87d7064a3c5e55af1e27f2cb2db9875c4133
caneel_7 <caneel.macwan@ascendtis.com>
2024-09-01 13:52:11 +0530

    inital commit: connected to mysqldatabase

---
+++ src/main/java/com/caneel/jmain/JmainApplication.java
@@ -0,0 +1,13 @@
+package com.caneel.jmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caneel/jmain/JmainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caneel.jmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
